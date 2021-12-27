33e78fb38e8ced3ba03366b2cad5452a14d92c49
NadaMarmouch <76530614+NadaMarmouch@users.noreply.github.com>
2021-12-27 17:19:53 +0200

    add

---
+++ courses.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Courses</title>
+<link rel="stylesheet" href="assets/navbar.css">
+<link rel="stylesheet" href="assets/courses_styles.css">
+<link rel="stylesheet" href="assets/Contact_us_styles.css"><!-- linking the styles-->
+    <script type="text/javascript" src="js/Contact_us_js.js" defer> </script>
+
+    
+<main>
+<?php include("templates/navbar.php")  ?> 
+
+<body>
+
+
+<button class="button">Computer Science</button>
+<button class="button">Alsun</button>
+<button class="button">Business Adminstration</button>
+<button class="button">Density</button>
+<button class="button">Pharmacy</button>
+<button class="button">Mass Communication</button>
+<button class="button">Architecture</button>
+<button class="button">Electronics</button>
+</body>
+</html>
+<?php include("Contact_us.php")  ?> <!-- linking the footer-->
+       <?php include("includes/Contact_us_inc.php")  ?> <!-- linking the footer-->
