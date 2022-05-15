818ad0e848e9d2f43706a50b345fc28d21dbd308
Philip-G-Hanna <philip1805061@miuegypt.edu.eg>
2022-05-15 23:08:45 +0200

    Question Bank

--- classAssignments.php
+++ classAssignments.php
@@ -10,10 +10,10 @@ class Assignment{
     $this->conn = $this->db->connection();
   }
 
-  public function addQuestion($question){
-  print("Question : $question");  
+  public function addQuestionModelAnswer($question,$answer){
+  // print("Question : $question");  
 
-  $query = "SELECT `text` FROM `questions` WHERE `text` = '".$question."'";
+  $query = "SELECT `text` FROM `questions` WHERE `text` = '".$question."' AND `modelanswer` = '".$answer."'";
   $result = mysqli_query($this->conn, $query);
 
   if(mysqli_num_rows($result) >= 1) {
@@ -21,7 +21,7 @@ class Assignment{
   }
   else if(mysqli_num_rows($result) <= 1)
   {
-    $query = "INSERT INTO `questions`(`text`) VALUES ('".$question."')";
+    $query = "INSERT INTO `questions`(`text`,`modelanswer`) VALUES ('".$question."','".$answer."')";
     $result = mysqli_query($this->conn, $query);
     if($result)
     {
@@ -32,23 +32,4 @@ class Assignment{
 }
 }
 
-?>
-<?php//   if($question->existsInDatabase())
-//   {
-//     echo '<script>alert("You already added this question")</script>';
-//   }
-//   else
-//   {
-//     mysqli_query($conn, "INSERT INTO questions (text) VALUES ('$question')");
-//     echo '<script>alert("Done, Questions have been added successfully")</script>';    
-//     return;
-//   }
-// }
-// public function addAnswer($email, $answer){
-//   include 'dB.php';
-//   mysqli_query($conn, "INSERT INTO Answers (email, answer) VALUES ('$email', '$answer')");
-//   echo '<script>alert("Done, Your answers have been submitted successfully")</script>';
-//   header("Refresh: 0.1");    
-//   return;
-//   }
 ?>
--- questionbank.php
+++ questionbank.php
@@ -9,7 +9,7 @@ include "assets/navbar.css";
 include 'classAssignments.php';
   if(isset($_POST['sub'])){
     $assignment = new Assignment();  
-    $assignment->addQuestion($_POST['txt']);
+    $assignment->addQuestionModelAnswer($_POST['txt'],$_POST['txt1']);
    } 
 ?> 
 <html>
@@ -17,18 +17,20 @@ include 'classAssignments.php';
     <div class="cc"><?php include "templates/navbar.php"?></div>
     <div class="register">
         <form method="POST" action="questionbank.php">
-            <input type="text" placeholder="Write the question here" name="txt" required><span> +</span><br>
+            <input type="text" placeholder="Write the question here" name="txt" required>
+            <input type="text" placeholder="Write the model answer here" name="txt1" required>
+            <!-- <span> +</span><br> -->
             <input type="submit" name="sub" value="Add Questions">
             <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 </div>
-    <script>
+    <!-- <script>
     $(document).ready(function(){
         "use strict";
         $(".input-file").delegate("span", "click", function(){
        $("<br><input type='text' placeholder='Write the question here' name='arr'><span> +</span>").insertAfter($(this)); 
     });
     }); 
-    </script>
+    </script> -->
         </form>
 </div>
     </body>
